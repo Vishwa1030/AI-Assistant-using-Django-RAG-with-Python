18518acc9397c778e7de53c83b730c45c1bf9f7a
Vishwa <129863796+Vishwa1030@users.noreply.github.com>
2026-01-18 13:21:07 +0530

    Add files via upload

---
+++ app/chat/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Thread, Message, Document
+
+@admin.register(Thread)
+class ThreadAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'title', 'created_at']  # ✅ Fixed
+    list_filter = ['user', 'created_at']
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ['id', 'thread', 'from_user', 'text', 'created_at']
+    list_filter = ['thread', 'from_user', 'created_at']
+
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'title', 'created_at'] 
+    list_filter = ['user', 'created_at']                 
+
